bc242eb9a8565807406cb2ef36c7c0d9a72b7733
Victor Chandra <victorchandra0911@gmail.com>
2024-12-13 02:30:58 +0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  staticPageGenerationTimeout: 1000,
   images: {
     remotePatterns: [
       // {
