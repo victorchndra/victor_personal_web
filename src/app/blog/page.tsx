fe32266c0bbc497afaf66663bda35f6620d990d0
Victor Chandra <victorchandra0911@gmail.com>
2024-12-13 01:46:35 +0700

    fix await

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,16 +3,33 @@ import BlogLayout from '../components/blog/blog-layout'
 // import BlogPosts from '../components/blog/blog-posts'
 import { getAllBlogPosts } from './actions'
 import HydrationWrapper from '../components/blog/hydration-wrapper'
+import { TContent } from './types';
 
-export default async function BlogCategory() {
-  const { data: lastestPosts } = await getAllBlogPosts()
+export async function getStaticProps() {
+  try {
+    const { data: latestPosts } = await getAllBlogPosts();
 
-  const posts = lastestPosts || []
+    return {
+      props: {
+        latestPosts: latestPosts || [], // Fallback to an empty array if undefined
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching blog posts:", error);
+    return {
+      props: {
+        latestPosts: [],
+      },
+    };
+  }
+}
+
+export default async function BlogCategory({ lastestPosts }: { lastestPosts: TContent[] }) {
 
   return (
     <BlogLayout>
       {/* <BlogPosts posts={lastestPosts} /> */}
-      <HydrationWrapper ssrData={posts} />
+      <HydrationWrapper ssrData={lastestPosts} />
     </BlogLayout>
   )
 }
--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -3,16 +3,33 @@ import ProjectLayout from '../components/project/project-layout'
 // import ProjectPosts from '../components/project/project-posts'
 import HydrationWrapper from '../components/project/hydration-wrapper'
 import { getAllProjectPosts } from './actions'
+import { TContent } from './types';
 
-const ProjectCategory = async () => {
-  const { data: recentProjects } = await getAllProjectPosts()
+export async function getStaticProps() {
+  try {
+    const { data: recentProjects } = await getAllProjectPosts();
 
-  const projects = recentProjects || []
+    return {
+      props: {
+        recentProjects: recentProjects || [], // Fallback to an empty array if undefined
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching blog posts:", error);
+    return {
+      props: {
+        recentProjects: [],
+      },
+    };
+  }
+}
+
+const ProjectCategory = async ({ recentProjects }: { recentProjects: TContent[] }) => {
 
   return (
     <ProjectLayout>
       {/* <ProjectPosts projects={projects} /> */}
-      <HydrationWrapper ssrData={projects} />
+      <HydrationWrapper ssrData={recentProjects} />
     </ProjectLayout>
   )
 }
