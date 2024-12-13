46a6a0af47926c0e7a0b2ec024022cef92d96133
Victor Chandra <victorchandra0911@gmail.com>
2024-12-14 01:12:09 +0700

    try change action cache

--- src/app/project/actions.ts
+++ src/app/project/actions.ts
@@ -4,7 +4,7 @@ export async function getAllProjectCategories() {
   try {
     const res = await fetch(`${process.env.HOST_API}/projectCategory`, {
       method: "GET",
-      cache: "no-store"
+      cache: "no-cache"
     })
 
     return res.json()
@@ -17,7 +17,7 @@ export async function getAllProjectPosts() {
   try {
     const res = await fetch(`${process.env.HOST_API}/project`, {
       method: "GET",
-      cache: "no-store"
+      // cache: "no-store"
     })
 
     return res.json()
@@ -30,7 +30,7 @@ export async function getFilteredProjectPosts($params: string) {
   try {
     const res = await fetch(`${process.env.HOST_API}/project/${$params}`, {
       method: "GET",
-      cache: "no-store"
+      // cache: "no-store"
     })
 
     return res.json()
@@ -43,7 +43,7 @@ export async function getProjectPost($category: string, $project: string) {
   try {
     const res = await fetch(`${process.env.HOST_API}/project/${$category}/${$project}`, {
       method: "GET",
-      cache: "no-store"
+      // cache: "no-store"
     })
 
     return res.json()
