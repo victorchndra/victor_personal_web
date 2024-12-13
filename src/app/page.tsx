110a57e51bbc45cbb4516e7d3e165a5f1334bd59
Victor Chandra <victorchandra0911@gmail.com>
2024-12-13 09:58:25 +0700

    add force-dynamic & revalidate home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import { LetsConnect } from "./components/lets-connect";
 import { ProfileSection } from "./components/profile-section";
 import { RecentProjects } from "./components/recent-projects";
 
+export const revalidate = 60;
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   return (
     <>
